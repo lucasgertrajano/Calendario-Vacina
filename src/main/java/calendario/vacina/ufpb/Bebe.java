65f4992132826badce4d689c8dc32f77b748152e
oJuanCarlos <juan.pacifico@dcx.ufpb.br>
2023-04-20 20:53:48 -0300

    Vacinas 2.0 dem dev

--- src/main/java/calendario/vacina/ufpb/Bebe.java
+++ src/main/java/calendario/vacina/ufpb/Bebe.java
@@ -63,40 +63,4 @@ public class Bebe{
         this.vacinasTomadas = vacinasTomadas;
     }
 
-    public boolean cadastrarBebe(Bebe bebe){
-        for(Bebe b : this.bebes) {
-            if(b.equals(bebe)) {
-                return false;
-            }
-        }
-        this.bebes.add(bebe);
-        return true;
-    }
-    public boolean removerBebe(Bebe bebe){
-        for(Bebe b : this.bebes) {
-            if(b.equals(bebe)) {
-                return false;
-            }
-        }
-        this.bebes.remove(bebe);
-        return true;
-    }
-    public Bebe pesquisarPeloNome(String nome) {
-        for( Bebe b: this.bebes) {
-            if(b.getNome().equals(nome)) {
-                return b;
-            }
-        }
-        return null;
-    }
-    public boolean alterarBebe(String nome, Bebe novoBebe){
-        for (int i = 0; i < this.bebes.size(); i++){
-            if (this.bebes.get(i).getNome().equals(nome)){
-                this.bebes.set(i, novoBebe);
-                return true;
-            }
-        }
-        return false;
-    }
-
 }
---
+++ src/main/java/calendario/vacina/ufpb/service/BebeService.java
@@ -0,0 +1,49 @@
+package calendario.vacina.ufpb.service;
+
+import calendario.vacina.ufpb.Bebe;
+import calendario.vacina.ufpb.Vacina;
+
+import java.util.ArrayList;
+
+public class BebeService {
+    private ArrayList<Vacina> vacinasRecomendadas;
+    private ArrayList<Vacina> vacinasTomadas;
+    private ArrayList<Bebe> bebes;
+    private Bebe bebe;
+
+    public boolean cadastrarBebe(Bebe bebe){
+        for(Bebe b : this.bebes) {
+            if(b.equals(bebe)) {
+                return false;
+            }
+        }
+        this.bebes.add(bebe);
+        return true;
+    }
+    public boolean removerBebe(Bebe bebe){
+        for(Bebe b : this.bebes) {
+            if(b.equals(bebe)) {
+                return false;
+            }
+        }
+        this.bebes.remove(bebe);
+        return true;
+    }
+    public Bebe pesquisarPeloNome(String nome) {
+        for( Bebe b: this.bebes) {
+            if(b.getNome().equals(nome)) {
+                return b;
+            }
+        }
+        return null;
+    }
+    public boolean alterarBebe(String nome, Bebe novoBebe){
+        for (int i = 0; i < this.bebes.size(); i++){
+            if (this.bebes.get(i).getNome().equals(nome)){
+                this.bebes.set(i, novoBebe);
+                return true;
+            }
+        }
+        return false;
+    }
+}
